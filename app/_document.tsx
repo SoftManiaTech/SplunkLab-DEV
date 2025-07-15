bd1ab26bdde2c79e55ddb535a090ca667eb97f87
ramachandran <mikeyram35@gmail.com>
2025-07-15 11:21:20 +0530

    modified document.tsx file related to gtag

--- app/_document.tsx
+++ app/_document.tsx
@@ -5,29 +5,28 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Google tag (gtag.js) */}
-        <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${GA_TRACKING_ID}');
-            `,
-          }}
-        />
-
         {/* Open Graph Metadata */}
-        <meta property="og:title" content="Splunk Lab - Soft Mania | Budget-Friendly Splunk Environments" />
-        <meta property="og:description" content="Launch and manage Standalone, Distributed, and Clustered Splunk labs within your budget and timeframe. Splunk Lab - Soft Mania offers quick, cost-effective deployment solutions for learning, development, and testing." />
-        <meta property="og:image" content="https://splunklab.softmania.in/Splunk-Lab-Wizard.png" />
+        <meta
+          property="og:title"
+          content="Splunk Lab - Soft Mania | Budget-Friendly Splunk Environments"
+        />
+        <meta
+          property="og:description"
+          content="Launch and manage Standalone, Distributed, and Clustered Splunk labs within your budget and timeframe. Splunk Lab - Soft Mania offers quick, cost-effective deployment solutions for learning, development, and testing."
+        />
+        <meta
+          property="og:image"
+          content="https://splunklab.softmania.in/Splunk-Lab-Wizard.png"
+        />
         <meta property="og:url" content="https://splunklab.softmania.in" />
         <meta property="og:type" content="website" />
 
         <link rel="canonical" href="https://splunklab.softmania.in" />
         <link rel="author" href="https://softmania.in" />
-        <link rel="me" href="https://www.linkedin.com/company/softmania-tech/" />
+        <link
+          rel="me"
+          href="https://www.linkedin.com/company/softmania-tech/"
+        />
         <link rel="me" href="https://www.instagram.com/softmaniatech/" />
         <link rel="me" href="https://www.youtube.com/@SoftManiaTech" />
       </Head>
