5c989d2bde50d17c324740c154f9627c3fcc4076
Ramachandran <161134411+Ramachandra35@users.noreply.github.com>
2025-07-15 09:50:00 +0530

    Update gtag.ts

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -4,7 +4,7 @@ declare global {
   }
 }
 
-export const GA_TRACKING_ID = 'G-WHVVB84Z78' // replace with your ID
+export const GA_TRACKING_ID = 'G-NPCJTWCYDF' // replace with your ID
 
 // Standard GA event
 export const event = ({ action, params }: {
